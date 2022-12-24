2912029f3ef138bff8bf2222f46e8b74f686ee30
AK-Polash <polashk199@gmail.com>
2022-12-25 00:04:37 +0600

    firebase added with the ability of Write and Read

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import InputArea from "./components/InputArea";
 import TodosArea from "./components/TodosArea";
+import { getDatabase, push, ref, set, onValue } from "firebase/database";
 
 function App() {
   let [title, setTitle] = useState("");
   let [task, setTask] = useState("");
+  let [errorMsg, setErrorMsg] = useState("");
+  let [todo, setTodo] = useState([]);
+  let db = getDatabase();
 
   let handleInputTitle = (e) => {
     setTitle(e.target.value);
@@ -15,9 +19,37 @@ function App() {
   };
 
   let handleSubmit = () => {
-    console.log(title);
-    console.log(task);
+    if (!title) {
+      setErrorMsg("Enter todo Title");
+    } else if (!task) {
+      setErrorMsg("Enter todo Task");
+    } else {
+      set(push(ref(db, "Todos")), {
+        todoTitle: title,
+        todoTask: task,
+      })
+        .then(() => {
+          console.log("kaj hoise");
+        })
+        .catch((error) => {
+          console.log("Error: ", error);
+        });
+
+      setErrorMsg("");
+    }
   };
+
+  // Read data from databse:
+  useEffect(() => {
+    const todoRef = ref(db, "Todos");
+    onValue(todoRef, (snapshot) => {
+      let arr = [];
+      snapshot.forEach((item) => {
+        arr.push(item.val());
+      });
+      setTodo(arr);
+    });
+  }, []);
 
   return (
     <>
@@ -27,7 +59,20 @@ function App() {
           handleInputTask={handleInputTask}
           handleSubmit={handleSubmit}
         />
-        <TodosArea title={title} task={task} />
+
+        <div className="text-center text-red-500 font-dm font-bold text-2xl py-4">
+          {errorMsg}
+        </div>
+
+        <div className="flex flex-col items-center gap-y-[30px]">
+          {todo.map((item, index) => (
+            <TodosArea
+              key={index}
+              title={item.todoTitle}
+              task={item.todoTask}
+            />
+          ))}
+        </div>
       </div>
     </>
   );
--- src/components/TodosArea.js
+++ src/components/TodosArea.js
@@ -8,37 +8,35 @@ import TodoPostItem from "./TodoPostItem";
 const TodosArea = ({ title, task }) => {
   return (
     <>
-      <div className="flex flex-col items-center gap-x-[30px]">
-        <div className="w-[500px] mx-auto py-[10px] px-[20px] rounded-[10px] bg-gradient-to-r from-primary to-secondary">
-          <div className="pt-[20px]">
-            <Heading
-              title={title}
-              className="text-[32px] font-bold text-pure font-dm text-center"
-            />
-          </div>
-
-          <TodoPost className="font-dm">
-            <TodoPostItem
-              className="text-pure py-[40px] text-center"
-              post={task}
-            />
-          </TodoPost>
-
-          <Buttons className="flex justify-center items-center gap-x-1 font-dm pb-[20px]">
-            <ButtonItem
-              title="Delete"
-              className="w-[100px] py-[6px] text-dark bg-rapid rounded"
-            />
-            <ButtonItem
-              title="Edit"
-              className="w-[100px] py-[6px] text-dark bg-rapid rounded"
-            />
-            <ButtonItem
-              title="Share"
-              className="w-[100px] py-[6px] text-dark bg-rapid rounded"
-            />
-          </Buttons>
+      <div className="w-[500px] mx-auto py-[10px] px-[20px] rounded-[10px] bg-gradient-to-r from-primary to-secondary">
+        <div className="pt-[20px]">
+          <Heading
+            title={title}
+            className="text-[32px] font-bold text-pure font-dm text-center"
+          />
         </div>
+
+        <TodoPost className="font-dm">
+          <TodoPostItem
+            className="text-pure py-[40px] text-center"
+            post={task}
+          />
+        </TodoPost>
+
+        <Buttons className="flex justify-center items-center gap-x-1 font-dm pb-[20px]">
+          <ButtonItem
+            title="Delete"
+            className="w-[100px] py-[6px] text-dark bg-rapid rounded"
+          />
+          <ButtonItem
+            title="Edit"
+            className="w-[100px] py-[6px] text-dark bg-rapid rounded"
+          />
+          <ButtonItem
+            title="Share"
+            className="w-[100px] py-[6px] text-dark bg-rapid rounded"
+          />
+        </Buttons>
       </div>
     </>
   );
